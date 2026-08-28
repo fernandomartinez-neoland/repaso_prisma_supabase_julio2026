01910643c91ae33383347664f59300de5fbd5dc7
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-08-28 12:02:24 +0200

    feat: enhance setUserService with error handling and response structure

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,7 @@
 import {setUserService} from '../services/user.service'
 
 export async function setUserController(req:any, res:any){
-    res.send(await setUserService(req.body))
+const serviceRsponse=await setUserService(req.body)
+    
+    res.status(serviceRsponse.status).send(serviceRsponse.message)
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,36 @@
-import {prisma} from '../lib/prisma';
-export async function setUserService(req:any){
-    return req
+import { prisma } from '../lib/prisma';
+import { Prisma } from '../../generated/prisma/client';
+
+interface setUsers {
+    name: string,
+    email: string
+}
+export async function setUserService(req: setUsers) {
+    const { name, email } = req
+    try {
+
+        const setUser = await prisma.user.create({ data: { name, email } })
+        console.log(setUser)
+        return {
+            status: 201,
+            message: "usuario creado"
+        }
+    } catch (e: any) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            // 2. Comprobamos el código P2002 (Restricción única fallida)
+            if (e.code === 'P2002') {
+                console.log("Error de duplicado en los campos:", e.meta?.target);
+                return {
+                    status: 409, // 409 Conflict es la mejor práctica para duplicados
+                    message: "El correo electrónico ya está registrado"
+                };
+            }
+        }
+        return {
+            status: 400,
+            message: "bad request"
+        }
+    }
+
+
 }
