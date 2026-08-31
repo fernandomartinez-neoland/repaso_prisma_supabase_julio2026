605c9ee2d5d14010aa967bfd30d293cbd1e97532
altrios <famartinez.ti@gmail.com>
2026-08-31 12:16:07 +0200

    feat: update User model to require password and enhance setUserService for better error handling

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,37 +1,39 @@
-import { prisma } from '../../lib/prisma';
-import { Prisma } from '../../../generated/prisma/client';
-
+import { prisma } from "../../lib/prisma";
+import { Prisma } from "../../../generated/prisma/client";
+import { hashPassword } from "../bcrypt/bcrypt.service";
 interface setUsers {
-    name: string,
-    email: string,
-    password: string
+  name: string;
+  email: string;
+  password: any;
 }
 export async function setUserService(req: setUsers) {
-    const { name, email, password } = req
-    try {
-
-        const setUser = await prisma.user.create({ data: { name, email } })
-        console.log(setUser)
+  const { name, email, password } = req;
+  try {
+    const hashPass = await hashPassword(password);
+    console.log("clave hasheada: ",hashPass)
+    const setUser = await prisma.user.create({
+      data: { name, email, "password": hashPass },
+    });
+    console.log(setUser);
+    return {
+      status: 201,
+      message: "usuario creado",
+    };
+  } catch (e: any) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // 2. Comprobamos el código P2002 (Restricción única fallida)
+      if (e.code === "P2002") {
+        console.log("Error de duplicado en los campos:", e.meta?.target);
         return {
-            status: 201,
-            message: "usuario creado"
-        }
-    } catch (e: any) {
-        if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            // 2. Comprobamos el código P2002 (Restricción única fallida)
-            if (e.code === 'P2002') {
-                console.log("Error de duplicado en los campos:", e.meta?.target);
-                return {
-                    status: 409, // 409 Conflict es la mejor práctica para duplicados
-                    message: "El correo electrónico ya está registrado"
-                };
-            }
-        }
-        return {
-            status: 400,
-            message: "bad request"
-        }
+          status: 409, // 409 Conflict es la mejor práctica para duplicados
+          message: "El correo electrónico ya está registrado",
+        };
+      }
     }
-
-
+    console.log(e);
+    return {
+      status: 400,
+      message: "bad request",
+    };
+  }
 }
