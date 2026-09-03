dfe0add853fc8f5d3d0dafd5a43e7675d49c4e08
altrios <famartinez.ti@gmail.com>
2026-09-03 09:49:05 +0200

    chore: configure dotenv and update port to use environment variables with gitignore updates

--- index.ts
+++ index.ts
@@ -3,20 +3,22 @@ import express from 'express'
 import userRouter from './src/routes/user.route'
 import invoiceRouter from './src/routes/invoice.route'
 import { userMiddleware } from './src/middleware/middleware'
+import dotenv from 'dotenv'
+dotenv.config()
 
 // config
 const app = express()
 app.use(express.json())
-const port = 3000
+const port = process.env.PORT;
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Hola mundo!")
 })
 
 app.use('/api/user', userRouter)
-app.use('/api/invoices',userMiddleware, invoiceRouter )
+app.use('/api/invoices', userMiddleware, invoiceRouter)
 
-app.listen(port, ()=>{
-    console.log("http://localhost:"+port)
+app.listen(port, () => {
+    console.log("http://localhost:" + port)
 })
